fddb34b7e65e56627633a8ff3e8e1d25aa17c845
Zachary Kay <zkay.ca@gmail.com>
2022-01-12 16:34:03 -0800

    Less strict TS rules for env variables for Auth0

--- src/index.tsx
+++ src/index.tsx
@@ -7,26 +7,22 @@ import { FilterProvider } from "./context/filter_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { UserProvider } from "./context/user_context";
 
-let Auth0Domain;
-let Auth0ClientId;
-const Auth0VariableErrorMsg = (envVariable) =>
-  `process.env variable for Auth0 ${envVariable} was not supplied to Auth0 Provider`;
+// let Auth0Domain: string | undefined;
+// let Auth0ClientId: string | undefined;
+// const Auth0VariableErrorMsg = (envVariable: string): string =>
+//   `process.env variable for Auth0 ${envVariable} was not supplied to Auth0 Provider`;
 
-if (process.env.REACT_APP_AUTH_DOMAIN) {
-  Auth0Domain = process.env.REACT_APP_AUTH_DOMAIN;
-} else {
-  throw new Error(Auth0VariableErrorMsg("domain"));
-}
-if (process.env.REACT_APP_AUTH_CLIENT_ID) {
-  Auth0ClientId = process.env.REACT_APP_AUTH_CLIENT_ID;
-} else {
-  throw new Error(Auth0VariableErrorMsg("clientId"));
-}
+// if (process.env.REACT_APP_AUTH_DOMAIN) {
+//   Auth0Domain = process.env.REACT_APP_AUTH_DOMAIN;
+// }
+// if (process.env.REACT_APP_AUTH_CLIENT_ID) {
+//   Auth0ClientId = process.env.REACT_APP_AUTH_CLIENT_ID;
+// }
 
 ReactDOM.render(
   <Auth0Provider
-    domain={Auth0Domain}
-    clientId={Auth0ClientId}
+    domain={process.env.REACT_APP_AUTH_DOMAIN!}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID!}
     redirectUri={window.location.origin}
     callbackUrl={window.location.origin}
     responseType="token id_token"
