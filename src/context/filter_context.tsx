54e3aa1d429b3ad0ae9abfded3617b1eefdc4d73
Zachary Kay <zkay.ca@gmail.com>
2022-01-12 21:08:40 -0800

    test settings

--- src/context/filter_context.tsx
+++ src/context/filter_context.tsx
@@ -3,7 +3,7 @@ import { ActionKind } from "../reducers/filter_reducer";
 
 import React, { FC, useEffect, useContext, useReducer } from "react";
 
-import axios from "axios";
+// import axios from "axios";
 
 interface FormData {
   name: string | undefined;
@@ -82,20 +82,73 @@ export const FilterProvider: FC = ({ children }) => {
   const [ state, dispatch ] = useReducer(reducer, initialStore);
 
   const fetchData = async (url: string) => {
+    // let headers = new Headers();
+
+    // headers.append('Content-Type', 'application/json');
+    // headers.append('Accept', 'application/json');
+    // headers.append('Authorization', 'Basic');
+    // headers.append('Origin', 'http://localhost:3000');
+
+    // interface Error {
+    //   status?: number;
+    //   message?: string;
+    // }
+
+    
+
     try {
-      const response = await axios.get(url, {
-        headers:
-          { "Content-Type": "application/json", "X-Requested-With": "XMLHttpRequest" },
-      });
-      const data = await response.data;
+      const response = await fetch(url,  {method: 'GET', headers: {"Content-Type": "application/json",
+      "Accept": "application/json", "Authorization": "Basic", "Origin": "http://localhost:3000",}, mode: 'cors'});
+      const data = await response.json();
       dispatch({ type: ActionKind.GET_DATA_SUCCESS, payload: data });
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
     } catch (err) {
-      console.log(err, `URL of: '${url}' was not properly defined`);
+      let errr = new Error("Not working");
+      console.log(err, `URL of: '${url}' requested`, errr);
     }
-  };
+  }
+      
+    //   fetch(sign_in, {
+    //     mode: 'cors',
+    //     credentials: 'include',
+    //     method: 'POST',
+    //     headers: headers
+    // })
+
+
+      
+      // const response = await axios.get(url, {
+      //   method: "GET",
+      //   headers:
+      //     {
+      //       "Content-Type": "application/json",
+      //       "Accept": "application/json",
+      //       "Authorization": "Basic",
+      //       // "Origin": "*",
+      //     },
+      //     mode: cors
+      
+  
+
+  //   function performSignIn() {
+
+  //     fetch(sign_in, {
+  //         mode: 'cors',
+  //         credentials: 'include',
+  //         method: 'POST',
+  //         headers: headers
+  //     })
+  //     .then(response => response.json())
+  //     .then(json => console.log(json))
+  //     .catch(error => console.log('Authorization failed: ' + error.message));
+  // }
 
   // const apiDomain: string | undefined = process.env.API_DOMAIN;
-  const domain: string = "https://euro-tours-api.netlify.app/api";
+  const domain: string = "https://euro-tours-api.herokuapp.com/api";
+  // const testDomain: string = 'http://localhost:4000/api'
 
   // Fetch Data
   useEffect(
